beb69c2d9488925788d7e293bbc0952cfd13fb5d
Faruq Hossain <faruqdigital01@gmail.com>
2023-06-12 16:17:02 +0600

    frontend course list

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  mode: "jit",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      baloo: ['"Baloo Da 2"'],
+      poppins: "Poppins",
+      bangla: "Hind Siliguri",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
